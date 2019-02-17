84d3f353dca1a19105d0d6051ea4943ab16e0e00
Babak Tourani <babak4@yahoo.com>
2019-02-17 00:54:56 +0000

    Adding Telegraf agent to MongoDB vagrant box

--- vagrant_boxes/mongoDB/files/provision.sh
+++ vagrant_boxes/mongoDB/files/provision.sh
@@ -1,7 +1,11 @@
-cd /tmp
-sudo cp mongodb-org-4.0.repo /etc/yum.repos.d/mongodb-org-4.0.repo
+sudo cp /tmp/influxdb.repo /etc/yum.repos.d/influxdb.repo
+sudo yum install -y telegraf
+sudo cp /tmp/mongodb-org-4.0.repo /etc/yum.repos.d/mongodb-org-4.0.repo
 sudo yum install -y mongodb-org
-sudo cp mongod.conf /etc/mongod.conf
+sudo cp /tmp/mongod.conf /etc/mongod.conf
 sudo service mongod start
+sudo cp /tmp/telegraf.conf /etc/telegraf/telegraf.conf
+sudo systemctl enable telegraf
+sudo systemctl start telegraf
 
 
